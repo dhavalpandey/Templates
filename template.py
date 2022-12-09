bad190def723d7a64a374dad0cb7e99b5f69f859
Dhaval Pandey <68287498+dhavalpandey@users.noreply.github.com>
2022-12-09 19:45:28 +0000

    Python Template

---
+++ template.py
@@ -0,0 +1,11 @@
+with open('../../Inputs/Day 9/Day9Input.txt', 'r') as f:
+    input_file = f.read().strip().split("\n")
+
+def solve():
+  #Write code here
+    
+def main():
+  solve()
+
+if __name__ == '__main__':
+  main()
